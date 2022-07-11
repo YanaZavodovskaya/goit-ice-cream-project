0fac38ed44b6ace2a4fc923efa30b631f31b9f0f
YanaZavodovskaya <104728312+YanaZavodovskaya@users.noreply.github.com>
2022-07-11 14:53:25 +0000

    Deploying to gh-pages from @ YanaZavodovskaya/goit-ice-cream-project@fcc852db8d2794ef5d988088012aa833b3aaf239 🚀

---
+++ index.53b3c0e9.js
@@ -0,0 +1,2 @@
+$(window).load((function(){var e=$("gallery__container");e.imagesLoaded((function(){e.masonry({columnWidth:".gallery__item",itemSelector:".gallery__tem"})}))}));
+//# sourceMappingURL=index.53b3c0e9.js.map
