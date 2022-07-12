9b5f895067bb579535df3f8e0e6e7a0c31b32b9d
YanaZavodovskaya <104728312+YanaZavodovskaya@users.noreply.github.com>
2022-07-12 06:33:31 +0000

    Deploying to gh-pages from @ YanaZavodovskaya/goit-ice-cream-project@15046ac14eebf4488939e2c60f6312eca3e1f844 🚀

--- index.62f3184d.js
+++ index.62f3184d.js
@@ -1,2 +1,2 @@
 $(window).load((function(){var e=$("gallery__container");e.imagesLoaded((function(){e.masonry({columnWidth:".gallery__item",itemSelector:".gallery__tem"})}))}));
-//# sourceMappingURL=index.53b3c0e9.js.map
+//# sourceMappingURL=index.62f3184d.js.map
--- index.9a42c839.js
+++ index.9a42c839.js
@@ -1,2 +1,2 @@
 var swiper=new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0}});
-//# sourceMappingURL=index.e7a167a9.js.map
+//# sourceMappingURL=index.9a42c839.js.map
